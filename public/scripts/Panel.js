41d5cdb495eb4bbacea74fb226eaddb0263b12e6
Hyuni <xuzaixian@sina.com>
2016-07-05 15:06:15 +0800

    可以联机发送并且获取画画信息，优化性能，流畅许多。

--- public/scripts/Panel.js
+++ public/scripts/Panel.js
@@ -1,47 +1,76 @@
 class Panel {
-    constructor(dom) {
+    constructor(dom, io) {
         this.el = document.getElementById(dom);
         this.ctx = this.el.getContext('2d');
         this.x = 0;
         this.y = 0;
         this.drawer = true;
         this.drawing = false;
+        this.ctx.strokeStyle = 'black';
+        this.ctx.lineWidth = 1;
         this.addListener();
+        this.io = io;
     }
     handleDown(e) {
-        if(drawer){
+        if (!this.drawer) {
             return
         }
         this.drawing = true;
         this.x = e.pageX - e.target.offsetLeft;
         this.y = e.pageY - e.target.offsetTop;
-        this.ctx.moveTo(this.x,this.y);
+        this.ctx.moveTo(this.x, this.y);
+        this.ctx.beginPath();
+        this.io.startDraw();
     }
     handleUp(e) {
-        if(drawer){
+        if (!this.drawer) {
             return
         }
-        this.drawing = false;
+        this.stopDrawing();
+        this.io.endDraw();
     }
     handleMove(e) {
-        if(drawer){
+        if (!this.drawer) {
             return
         }
-        if(!this.drawing){
+        if (!this.drawing) {
             return;
         }
-        this.x = e.pageX - e.target.offsetLeft;
-        this.y = e.pageY - e.target.offsetTop;
+        this.x = Math.floor(e.pageX - e.target.offsetLeft);
+        this.y = Math.floor(e.pageY - e.target.offsetTop);
         this.ctx.lineTo(this.x, this.y);
         this.ctx.stroke();
+        this.io.sendDraw(this.x, this.y);
+    }
+    stopDrawing() {
+        this.drawing = false;
+        this.ctx.closePath();
+    }
+    remoteStartDraw(x, y) {
+        console.log('start');
+        this.ctx.moveTo(x, y);
+        this.ctx.beginPath();
+    }
+    remoteDraw(x, y) {
+        console.log(x,'+',y);
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
+    }
+    remoteEndDraw() {
+        console.log('end');
+        this.ctx.closePath();
     }
     addListener() {
         this.el.addEventListener("mousedown", event => this.handleDown(event));
         this.el.addEventListener("mouseup", event => this.handleUp(event));
         this.el.addEventListener("mousemove", event => this.handleMove(event));
     }
+    setColor(color) {
+        this.ctx.strokeStyle = color;
+    }
+    setWidth(width) {
+        this.ctx.lineWidth = width;
+    }
 }
 
 module.exports = Panel;
-
-var ctx = document.getElementById('panel').getContext('2d');
