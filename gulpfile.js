5145b082aecb7630a90da38ccc18f6e827cca535
Hyuni <xuzaixian@sina.com>
2016-07-07 22:01:58 +0800

    update gitignore

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var connect = require('gulp-connect'),
+    gulp = require('gulp'),
+    less = require('gulp-less'),
+    gutil = require('gulp-util');
+    webpack = require('webpack');
+
+gulp.task('webserver', function () {
+    connect.server({
+        root: './',
+        livereload: true
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./views/*.html', ['html-reload'])
+    gulp.watch('./public/stylesheets/*.less', ['less-reload'])
+    gulp.watch('./public/scripts/*.js', ['js-reload'])
+});
+gulp.task('html-reload', function () {
+    gulp.src('./views/*.html')
+        .pipe(connect.reload());
+});
+gulp.task('less-reload', ['less'],function () {
+    gulp.src('./public/stylesheets/*.css')
+        .pipe(connect.reload());
+});
+gulp.task('js-reload', ['webpack'],function () {
+    gulp.src('./public/scripts/dist/*.js')
+        .pipe(connect.reload());
+});
+gulp.task('less', function (cb) {
+    gulp.src('./public/stylesheets/*.less')
+    .pipe(less({
+        path: './public/stylesheets/'
+    }))
+    .pipe(gulp.dest('./public/stylesheets/dist'));
+    cb();
+});
+gulp.task('webpack', function (cb) {
+    var config = {
+        entry: "./public/scripts/client.js",
+        output: {
+            path: "./public/scripts/dist",
+            filename: "game-script.js"
+        }
+    };
+    webpack(config, function (err, stats) {
+        if (err) {
+            gutil.log(err);
+        } else {
+            gutil.log(stats.toString());
+        }
+    });
+    cb();
+});
